5459ed4b245e90c5fc4d55e67c071400d6bc1129
Lucas Stopinski da Silva <lucasstopinskidasilva@gmail.com>
2024-08-13 19:37:31 -0300

    Organiação

---
+++ Aula de Testes - 31-07/Código.sql
@@ -0,0 +1,9 @@
+/* Lógico_1: */
+
+CREATE TABLE pessoa (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(50),
+    Sobrenome VARCHAR(50),
+    CPF VARCHAR(20),
+    Data_Nasc DATE
+);
---
+++ Aula de Testes - 31-07/Registro.sql
@@ -0,0 +1,1 @@
+SELECT * FROM primeiro_teste.pessoa;
